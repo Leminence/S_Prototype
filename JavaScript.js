3db4f979e8204340881f881e102b083897c4fae3
lema.repository@gmail.com <lema.repository@gmail.com>
2023-02-04 19:56:19 -0300

    v1

---
+++ JavaScript.js
@@ -0,0 +1,11 @@
+function openNav() {
+   document.getElementById("meumenu").style.marginLeft = "-2%";
+   document.getElementById("openbtn").style.display = "none";
+   document.getElementById("closebtn").style.display = "block";
+   
+}
+function closeNav() {
+   document.getElementById("meumenu").style.marginLeft = "-62%";
+   document.getElementById("openbtn").style.display = "block";
+   document.getElementById("closebtn").style.display = "none";
+}
